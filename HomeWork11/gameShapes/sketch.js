728d34f35e3e8eb48c6f2c00edf2b2d5d6aaab30
rlaObear <rhonda1.aubert@umontana.edu>
2023-12-03 16:57:13 -0700

    h

--- HomeWork11/gameShapes/sketch.js
+++ HomeWork11/gameShapes/sketch.js
@@ -1,138 +1,133 @@
-// x and y for my character
-var playerShapeX = 90;
-var PlayerShapeY = 90;
-// x and y for a shape
-var shapeX = 30;
-var shapeY = 50;
-var shapeXSpeed;
-var shapeYSpeed;
+// Starting location of player circle
+var playerX = 90;
+var playerY = 90;
 
-// key codes
-var w = 87; 
+// Defining key codes
+var w = 87;
 var s = 83;
 var a = 65;
 var d = 68;
 
-// x and y for a shape
-var shapeX = 30;
-var shapeY = 50;
-var shapeXSpeed;
-var shapeYSpeed;
+// Starting locations of villains
+var villainX = 30;
+var villainY = 50;
+var villainXSpeed;
+var villainYSpeed;
 
-// create a shape when the mouse is clicked
+var villain2X = 100;
+var villain2Y = 200;
+var villain2XSpeed;
+var villain2YSpeed;
+
+// Shape created when mouse is clicked.
 var mouseShapeX;
 var mouseShapeY;
-function setup()
-{
+
+function setup() {
     createCanvas(600, 700);
-    // get a random speed when the it first starts
-    shapeXSpeed = Math.floor(Math.random() * (Math.floor(Math.random() * 5)) + 1);
-    shapeYSpeed = Math.floor(Math.random() * (Math.floor(Math.random() * 5)) + 1);
+    // Get random speeds when it first starts for both villains
+    villainXSpeed = Math.floor(Math.random() * (Math.floor(Math.random() * 5)) + 1);
+    villainYSpeed = Math.floor(Math.random() * (Math.floor(Math.random() * 5)) + 1);
+    
+    villain2XSpeed = Math.floor(Math.random() * (Math.floor(Math.random() * 5)) + 1);
+    villain2YSpeed = Math.floor(Math.random() * (Math.floor(Math.random() * 5)) + 1);
 }
 
-function draw()
-{
-    background(22,249,245);
+function draw() {
+    // Borders
+    background(255, 255, 153);
     stroke(0);
     fill(0);
     // top border
-    rect(0,0,width,20);
+    rect(0,0,width,10);
     // left border
-    rect(0,0,20,height);
+    rect(0,0,10,height);
     // bottom border
-    rect(0, height-20,width, 20);
+    rect(0, height-10,width, 10);
     // right upper border
-    rect(width-20,0,20,height-50);
+    rect(width-10,0,10,height-50);
 
-    // exit message
-    textSize(15);
-    text("Get Out Here!", width-100,height-30)
+    // Escape message
+    textSize(14);
+    text("Escape Here!", width - 100, height - 30);
 
-    //character
-    fill(255,23,252);
-    circle(playerShapeX,PlayerShapeY,35);
+    // Player shape and color
+    fill(255, 23, 252);
+    circle(playerX, playerY, 35);
 
-    // handle the keys
-    if(keyIsDown(w))
-    {
-        playerShapeY -= 10;   
+    // W, S, A, and D key movements
+    if (keyIsDown(w)) {
+        playerY -= 10;
     }
-    if(keyIsDown(s))
-    {
-        playerShapeY += 10;   
+    if (keyIsDown(s)) {
+        playerY += 10;
     }
-    if(keyIsDown(a))
-    {
-        playerShapeX -= 10;   
+    if (keyIsDown(a)) {
+        playerX -= 10;
     }
-    if(keyIsDown(d))
-    {
-        playerShapeX += 10;   
+    if (keyIsDown(d)) {
+        playerX += 10;
     }
 
-    // potential enemy
-    fill(237,28,36);
-    // draw the shape
-    circle(shapeX, shapeY, 30);
+    // Villain 1
+    fill(0, 255, 0);
+    circle(villainX, villainY, 30);
 
-     // get a random speed when the it first starts
-     shapeXSpeed = Math.floor(Math.random() * (Math.floor(Math.random() * -10)) + -1);
-     shapeYSpeed = Math.floor(Math.random() * (Math.floor(Math.random() * -10)) + -1);
+    // Get random speed when it first starts
+    villainX += villainXSpeed;
+    villainY += villainYSpeed;
 
-    // move the shape
-    shapeX += shapeXSpeed;
-    shapeY += shapeYSpeed;
-    // check to see if the shape has gone out of bounds
-    if(shapeX > width)
-    {
-        shapeX = 0;
+    // Verify if the villain has gone out of bounds
+    if (villainX > width) {
+        villainX = 0;
     }
-    if(shapeX < 0)
-    {
-        shapeX = width;
+    if (villainX < 0) {
+        villainX = width;
     }
-    if(shapeY > height)
-    {
-        shapeY = 0;
+    if (villainY > height) {
+        villainY = 0;
     }
-    if(shapeY < 0)
-    {
-        shapeY = height;
+    if (villainY < 0) {
+        villainY = height;
+    }
+
+    // Villain 2
+    fill(145, 25, 255);
+    circle(villain2X, villain2Y, 40);
+
+    // Get random speed when it first starts
+    villain2X += villain2XSpeed;
+    villain2Y += villain2YSpeed;
+
+    // Verify if the villain has gone out of bounds
+    if (villain2X > width) {
+        villain2X = 0;
+    }
+    if (villain2X < 0) {
+        villain2X = width;
+    }
+    if (villain2Y > height) {
+        villain2Y = 0;
+    }
+    if (villain2Y < 0) {
+        villain2Y = height;
     }
 
     // check to see if the character has left the exit
-    if(playerShapeX > width && playerShapeY > width-50)
+    if(playerX > width && playerY > width-50)
     {
         fill(0);
         stroke(5);
-        textSize(45);
-        text("You Win!", width/2-60, height/2-60);
+        textSize(26);
+        text("You Win!", width/2-50, height/2-50);
     }
 
-    // create the shape based on the mouse click
-    fill(120,130,140);
+    // Create the shape based on the mouse click
+    fill(120, 130, 140);
     circle(mouseShapeX, mouseShapeY, 25);
 }
 
-function mouseClicked()
-{
+function mouseClicked() {
     mouseShapeX = mouseX;
     mouseShapeY = mouseY;
 }
-/*
-function keyPressed() {
-    if (keyCode === LEFT_ARROW) {
-        characterX -= 10;
-    } 
-    else if (keyCode === RIGHT_ARROW) {
-        characterX += 10;
-    }
-    else if (keyCode === UP_ARROW) {
-        characterY -= 10;
-    }
-    else if (keyCode === DOWN_ARROW) {
-        characterY += 10;
-    }
-
-  }
-  */
